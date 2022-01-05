8f9327079db7cfecb958706d8ad0998c207c2b43
patrick mugambi <patrick@kali.pm.com>
2022-01-06 01:03:00 +0300

    add accounts to enable user management

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("homepage.urls", namespace="homepage")),
     path("", include("users.urls", namespace="users")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
