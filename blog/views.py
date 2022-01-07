4f6a79764cc676e9d59eb9ce2e39534596b118f8
patrick mugambi <patrick@kali.pm.com>
2022-01-07 22:51:23 +0300

    blog view function

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,17 @@ def blogs(request):
     posts = BlogPost.objects.all()
     posts = BlogPost.objects.filter().order_by('-dateTime')
     return render(request, "blog.html", {'posts':posts})
+
+def add_blogs(request):
+    if request.method=="POST":
+        form = BlogPostForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            blogpost = form.save(commit=False)
+            blogpost.author = request.user
+            blogpost.save()
+            obj = form.instance
+            alert = True
+            return render(request, "add_blogs.html",{'obj':obj, 'alert':alert})
+    else:
+        form=BlogPostForm()
+    return render(request, "add_blogs.html", {'form':form})
