823c1ceb2cc6d2c5ebcb7f9a3b76d1f06d1f4e39
patrick mugambi <patrick@kali.pm.com>
2022-01-07 22:41:22 +0300

    set blog model and urls.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+class BlogPost(models.Model):
+    title=models.CharField(max_length=255)
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+    slug=models.CharField(max_length=130)
+    content=models.TextField()
+    image = models.ImageField(upload_to="blog_pics", blank=True, null=True)
+    dateTime=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.author) +  " Blog Title: " + self.title
+    
+    def get_absolute_url(self):
+        return reverse('blogs')
+    
