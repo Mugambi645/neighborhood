3cbd58ef72ebbe46ab2ee3e75dc9ca4b10eab3b2
patrick mugambi <patrick@kali.pm.com>
2022-01-08 03:20:22 +0300

    include comment page

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ app_name = "blog"
 urlpatterns = [
     path("blogs", views.blogs, name="blogs"),
     path("add_blogs", views.add_blogs, name="add_blogs"),
+    path('<slug:slug>/blog-comments', views.blog_comments, name="blog_comments"),
+    #re_path(r'^join/', views.join, name='join'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -26,3 +26,13 @@ def blogs_comments(request, id):
     post = BlogPost.objects.get(id = id)
     return render(request, "blog_comments.html", {'post':post}) 
 
+def blog_comments(request, slug):
+    post = BlogPost.objects.filter(slug=slug).first()
+    comments = Comment.objects.filter(blog=post)
+    if request.method=="POST":
+        user = request.user
+        content = request.POST.get('content','')
+        blog_id =request.POST.get('blog_id','')
+        comment = Comment(user = user, content = content, blog=post)
+        comment.save()
+    return render(request, "blog_comments.html", {'post':post, 'comments':comments}) 
